4690f2488385caff730a02fb5b828b1fe916bbb1
brody.sanford@gmail.com <brodysmac@Brodens-MBP.fios-router.home>
2022-05-18 15:22:04 -0400

    commit tree 3af9efe1c702cd641912dbbb8c5328bd76126954

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // code your solution here
+
+function superbowlWin(array) {
+    let winYear 
+    if (array.find(element => element.result === "W")) {
+        winYear = array.find(element => element.result === "W");
+        return winYear.year
+    }
+}
+/*
+function superbowlWin(array) {
+    let winYear
+    winYear = array.find(element => element.result === "W");
+    return winYear.year
+}
+
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    { year: "2012", result: "N/A"},
+    { year: "2011", result: "N/A"},
+    { year: "2010", result: "N/A"},
+    { year: "2009", result: "N/A"},
+    { year: "2008", result: "N/A"},
+    { year: "2007", result: "N/A"},
+    { year: "2006", result: "N/A"},
+    { year: "2005", result: "N/A"},
+    { year: "2004", result: "N/A"},
+    { year: "2003", result: "N/A"},
+    { year: "2002", result: "N/A"},
+    { year: "2001", result: "N/A"},
+    { year: "2000", result: "N/A"},
+    { year: "1999", result: "N/A"}
+  ]
+*/
